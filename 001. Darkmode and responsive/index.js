62e153f62a6b20b04a5912d288792cf898a82dab
tamhoangweb <tamhoangweb@gmail.com>
2021-05-17 18:57:27 +0700

    Chart js | Update...

--- 001. Darkmode and responsive/index.js
+++ 001. Darkmode and responsive/index.js
@@ -80,3 +80,57 @@ function switchTheme() {
   }
 }
 loadTheme();
+
+/* chart js */
+const primaryColor = "#4834d4";
+const warningColor = "#f0932b";
+const successColor = "#6ab04c";
+const dangerColor = "#eb4d4b";
+
+let ctx = document.getElementById("myChart");
+ctx.height = 500;
+ctx.width = 500;
+
+let data = {
+  labels: [
+    "Tháng 1",
+    "Tháng 2",
+    "Tháng 3",
+    "Tháng 4",
+    "Tháng 5",
+    "Tháng 6",
+    "Tháng 7",
+    "Tháng 8",
+    "Tháng 9",
+    "Tháng 10",
+    "Tháng 11",
+    "Tháng 12",
+  ],
+  datasets: [
+    {
+      fill: false,
+      label: "Completed",
+      borderColor: successColor,
+      data: [120, 115, 130, 100, 123, 88, 99, 66, 120, 52, 59],
+      borderWidth: 2,
+      // lineTension: 0,
+    },
+    {
+      fill: false,
+      label: "Issues",
+      borderColor: dangerColor,
+      data: [66, 44, 12, 48, 99, 56, 78, 23, 100, 22, 47],
+      borderWidth: 2,
+      // lineTension: 0,
+    },
+  ],
+};
+
+let lineChart = new Chart(ctx, {
+  type: "line",
+  data: data,
+  options: {
+    maintainAspectRatio: false,
+    bezierCurve: true,
+  },
+});
